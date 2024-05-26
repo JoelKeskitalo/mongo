c491695dd982025e29915b436e947777a3db5ba6
J0113boy <keskitalo_6@hotmail.com>
2024-05-26 09:15:22 +0200

    added a middleware for correct format for saved books

--- models/bookModel.js
+++ models/bookModel.js
@@ -5,9 +5,10 @@ const bookSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    author: {
-        type: String,
-        required: true
+    author: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Author', 
+        required: true },
     },
     year: {
         type: Number
@@ -15,6 +16,14 @@ const bookSchema = new mongoose.Schema({
     genre: {
         type: String
     }
+)
+
+// pre innebär att denna funktion körs automatiskt innan ett book-dokument sparas i databasen, därav PRE
+bookSchema.pre('save', function(next) {
+    this.title = this.title.split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join(' ')
+        next()
 })
 
 const Book = mongoose.model('Book', bookSchema)
